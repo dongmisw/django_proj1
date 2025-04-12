3ada1300c22c63057c9a78c0ce700b959a928b0b
heejinlee <heejinlee@dongyang.ac.kr>
2025-04-12 23:56:07 +0900

    post 새로 추가

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'[{self.pk : >4}] {self.title : >20}   {self.created_at : >40}       {self.updated_at}'
+    class Meta:
+        ordering = ['-created_at']
+
